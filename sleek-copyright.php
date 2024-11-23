e4295ba897b71698289d8ab20d55623e6066ce55
Shams Khan <robeeen@gmail.com>
2024-11-23 21:20:21 +0600

    Update sleek-copyright.php

--- sleek-copyright.php
+++ sleek-copyright.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Sleek Copyright
  * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.6
- * Requires PHP:      7.0
+ * Requires PHP:      8.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
